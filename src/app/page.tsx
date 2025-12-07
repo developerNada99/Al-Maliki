104df0871d6d9c3c51f9bbab40e5dbca3e73ad3d
NadaAhme <nadaahmed33470@gmail.com>
2025-12-07 06:48:53 +0200

    update package

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -109,7 +109,7 @@ export default function Home() {
           <h1 className="text-4xl md:text-6xl font-bold mb-7 max-md:text-4xl text-center">
             مرحبًا بك في مكتب الخدمات العامه
           </h1>
-          <p className="text-lg md:text-2xl mb-40 text-gray-400 text-center">
+          <p className="text-lg md:text-2xl mb-40 text-gray-400 text-center mx-20">
             نحن نقدم مجموعة متكاملة من الخدمات العامة التي تلبي احتياجات الأفراد والشركات، ونسعى دائمًا لتقديم حلول فعالة ومبتكرة تضمن رضا عملائنا وتعزز من جودة تجربتهم معنا
           </p>
 
